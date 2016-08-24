160ad99d09d68b7ac682d7fd0c8f98fa7ffa7030
Matthew Robertson <mrmatt1877@gmail.com>
2016-08-23 20:34:50 -0400

    added smooth scroll

--- js/index.js
+++ js/index.js
@@ -2,13 +2,13 @@ $(document).ready(function(e) {
   var width = $(document).width();
   if (width >= 699){
     $("#animate").css("right", -width)
-   
+
     $("#animate").animate({
       right: (width * .00005)
     }, 2500, function() {
       // Animation complete.
     });
-    
+
   function pulse() {
     var pulseAmount = 0;
     $('#old-school').animate({
@@ -23,9 +23,24 @@ $(document).ready(function(e) {
         });
     });
   };
-    
+
    setTimeout(pulse, 2500);
    setTimeout(pulse, 3200);
 
   }
+//smoothscroll
+  $(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
 });
