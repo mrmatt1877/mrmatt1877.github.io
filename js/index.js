32366bb0c2a139de0966949f2c17b7432ac3dd38
Matthew Robertson <mrmatt1877@gmail.com>
2016-08-26 23:50:23 -0400

    pushing new portfolio to github pages

--- js/index.js
+++ js/index.js
@@ -1,46 +1,10 @@
-$(document).ready(function(e) {
-  var width = $(document).width();
-  if (width >= 699){
-    $("#animate").css("right", -width)
+$( document ).ready(function(){
+  var options = [
+    {selector: '.material-icons', offset: 200, callback: function(el) {
+      Materialize.showStaggeredList($(el));
+     } }
+  ];
+  Materialize.scrollFire(options);
 
-    $("#animate").animate({
-      right: (width * .00005)
-    }, 2500, function() {
-      // Animation complete.
-    });
-
-  function pulse() {
-    var pulseAmount = 0;
-    $('#old-school').animate({
-      height: '550px'
-      }, 400, function() {
-      // First animate complete
-        $('#old-school').animate({
-          height: '468px'
-         }, 400, function() {
-          // Second animate complete
-           $('#old-school').removeAttr('style');
-        });
-    });
-  };
-
-   setTimeout(pulse, 2500);
-   setTimeout(pulse, 3200);
-
-  }
-//smoothscroll
-  $(function() {
-  $('a[href*="#"]:not([href="#"])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html, body').animate({
-          scrollTop: target.offset().top
-        }, 1000);
-        return false;
-      }
-    }
-  });
-});
-});
+  $(".button-collapse").sideNav();
+})
